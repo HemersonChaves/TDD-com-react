addde72e608775c8e04f9350fe59570eb094119a
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-16 18:52:50 -0300

    test(Formulario): quando o input está vazio, novos participantes não podem ser adicionados

---
+++ src/Formulario.test.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+// jest
+test('quando o input está vazio, novos participantes não podem ser adicionados', () =>{
+    render(<Formulario />)
+
+    //encontrar o input no DOM
+    const input = screen.getAllByPlaceholderText ('Insira os nomes dos participantes');
+    
+    //encontrar o botão no DOM
+    const botao = screen.getByRole('button')
+
+    // garantir que o input esteja no documento
+    expect(input).toBeInTheDocument();
+
+    // garantir que o botão esteja desabilitado no documento
+    expect(botao).toBeDisabled();
+
+})
