948af390582b664ffea9c8b4247dd180afb4354f
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-16 22:34:00 -0300

    refactor(Formulario): move componente Fomulario e cria index

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import Formulario from "./Formulario";
+import Formulario from ".";
 
 // jest
 test('quando o input está vazio, novos participantes não podem ser adicionados', () =>{
