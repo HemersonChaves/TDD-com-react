54517c6063ea79fd74e98ad3d057693792c9d47b
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-16 22:52:10 -0300

    refactor(formulario): aplicando estilos css

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Styles from "./styles.module.css"
 const Formulario = () => {
     return (
-    <form>
-        <input type='text' placeholder="Insira os nomes dos participantes" />
-        <button disabled={true}>Adicionar</button>
+    <form className={Styles.form}>
+        <input className={Styles.input} type='text' placeholder="Insira os nomes dos participantes" />
+        <button className={Styles.button} disabled={true}>Adicionar</button>
     </form>
     )
 }
