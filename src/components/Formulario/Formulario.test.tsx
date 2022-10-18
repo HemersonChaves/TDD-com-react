07ad1b6c4239d0aca4189c0775221630441f5cb3
Hemerson Chaves <hemersonchaves20@hotmail.com>
2022-10-18 14:27:23 -0300

    refactor(input): adiciona um participante com useRef e o foco no input

--- src/components/Formulario/Formulario.test.tsx
+++ src/components/Formulario/Formulario.test.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Formulario from ".";
+import { RecoilRoot } from "recoil";
 
 // jest
-test('quando o input está vazio, novos participantes não podem ser adicionados', () =>{
+test('quando o input está vazio, novos participantes não podem ser adicionados', () => {
     render(<Formulario />)
 
     //encontrar o input no DOM
-    const input = screen.getByPlaceholderText ('Insira os nomes dos participantes');
-    
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+
     //encontrar o botão no DOM
     const botao = screen.getByRole('button')
 
@@ -18,4 +19,36 @@ test('quando o input está vazio, novos participantes não podem ser adicionados
     // garantir que o botão esteja desabilitado no documento
     expect(botao).toBeDisabled();
 
+})
+
+test("adicionar um participante caso exista um nome preenchido", () => {
+    render(
+        <RecoilRoot>
+            <Formulario />
+        </RecoilRoot>
+    )
+
+    //encontrar o input no DOM
+    const input = screen.getByPlaceholderText('Insira os nomes dos participantes');
+
+    //encontrar o botão no DOM
+    const botao = screen.getByRole('button');
+
+    //inserir um valor no input
+    fireEvent.change(
+        input,
+        {
+            target: {
+                value: "Ana Catarina"
+            }
+        })
+
+    //clicar no botão de submiter
+    fireEvent.click(botao);
+
+    //garantir que o input esteja com o foco ativo
+    expect(input).toHaveFocus();
+    
+    // garantir que o input não tenha valor
+    expect(input).toHaveValue("")
 })
--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { useAdicionarParticipante } from '../../state/hooks/useAdicionarParticipante';
 import Styles from "./styles.module.css"
 const Formulario = () => {
+    const [nome, setNome] = useState("");
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const adicionarNaLista = useAdicionarParticipante();
+
+    const adicionarParticipante = (evento: React.FormEvent<HTMLFormElement>) => {
+        evento.preventDefault();
+        //cancela qualquer evento disparado
+        adicionarNaLista(nome)
+        setNome("")
+        inputRef.current?.focus()
+
+    }
     return (
-    <form className={Styles.form}>
-        <input className={Styles.input} type='text' placeholder="Insira os nomes dos participantes" />
-        <button className={Styles.button} disabled={true}>Adicionar</button>
-    </form>
+        <form
+            onSubmit={adicionarParticipante}
+            className={Styles.form}>
+            <input
+                ref={inputRef}
+                value={nome}
+                onChange={evento => setNome(evento.target.value)}
+                className={Styles.input}
+                type='text'
+                placeholder="Insira os nomes dos participantes" />
+            <button className={Styles.button} disabled={!nome}>Adicionar</button>
+        </form>
     )
 }
 
